2d6cee9cc45b560212713a92a8e4f44a4998b701
rer3378 <rer3378@rit.edu>
2018-10-17 13:52:53 -0400

    Testing permissions

--- src/patch.sh
+++ src/patch.sh
@@ -5,3 +5,5 @@ chmod +x $HOME/$YOUR_WS/src/rosaria/cfg/RosAria.cfg
 chmod +x $HOME/$YOUR_WS/src/mypkg/my_publisher.py
 chmod +x $HOME/$YOUR_WS/src/mypkg/my_subscriber.py
 echo "Setup complete.."
+
+#testing
